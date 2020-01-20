0f0cfbbc33c86e6ad79d558712adc5aa6aef00a9
Naman Tebak <naman.t@talview.com>
2020-01-20 11:26:18 +0530

    Constructors of objects

---
+++ definingobjects.js
@@ -0,0 +1,19 @@
+class Firstclass{
+    constructor(...args){
+        this['1']=args[0]
+        this.one=args[1]
+        this['2']=args[2]
+        this.three=args[3]
+        this.length=args.length
+    }
+    displaykeys(){
+        console.log(Object.keys(this))
+    }
+    display(){
+        console.log(this)
+    }
+}
+
+obj=new Firstclass('sad','qw',2,'edfw','e',3243,'sf','f',)
+obj.displaykeys()
+obj.display()
