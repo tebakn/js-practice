fdc56961f37d1dabf7d1527fea3a67e2953920c0
Naman <naman.t@talview.com>
2020-02-04 19:14:46 +0530

    API for post get delete with postgres

--- postgres/connect.js
+++ postgres/connect.js
@@ -1,6 +1,7 @@
 const Client =require("pg").Client
 var express = require('express');
 var app = express();
+app.use(express.json())
 
 
 
@@ -16,7 +17,59 @@ app.get("/listtable",(req,res)=>{
     client.connect()
     .then(()=>{console.log("WOHOO")})
     .then(()=>client.query("Select * from first_table limit 5;"))
+    .then(result=>{console.log(result.rows);res.send(JSON.stringify(result.rows))})
+    .catch((e)=>{console.log(e); client.end()})
+
+})
+
+app.get("/listtable/:searchparam",(req,res)=>{
+    client=new Client({
+        user: "postgres",
+        password:"1234",
+        host:"desk-naman",
+        port:5432,
+        database:"postgres"
+    })
+    search=req.params.searchparam.split(':')
+    client.connect()
+    .then(()=>{console.log("WOHOO")})
+    .then(()=>client.query(`Select * from first_table where ${search[0]}='${search[1]}';`))
     .then(result=>{console.log(result.rows);res.end(JSON.stringify(result.rows))})
+    .catch((e)=>{console.log(e); client.end()})
+
+})
+
+app.post("/listtable",(req,res)=>{
+    client=new Client({
+        user: "postgres",
+        password:"1234",
+        host:"desk-naman",
+        port:5432,
+        database:"postgres"
+    })
+    //console.log(req.body)
+    insobj=req.body.details
+    client.connect()
+    .then(()=>{console.log("WOHOO")})
+    .then(()=>{
+        if (insobj["email"]===undefined)
+            client.query(`Insert into first_table(first_name,last_name,gender,dob,country) \
+            values ('${insobj.first_name}','${insobj.last_name}','${insobj.gender}','${insobj.dob}','${insobj.country}');`)})
+    .then(()=>res.send("chal toh rha hai"))
+    .catch((e)=>{console.log(e); client.end()})
+})
+
+app.delete("/listtable/:id",(req,res)=>{
+    client=new Client({
+        user: "postgres",
+        password:"1234",
+        host:"desk-naman",
+        port:5432,
+        database:"postgres"
+    })
+    client.connect()
+    .then(()=>{console.log("WOHOO")})
+    .then(()=>client.query(`delete from first_table where id='${req.params.id}';`))
     .catch((e)=>{console.log(e); client.end()})
 
 })
